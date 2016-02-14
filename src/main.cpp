99995ed5b90c88815ad7eab9d5bcae4125a2dfbe
Chris Barrett <chris@codesaru.com>
2016-02-14 12:23:36 -0800

    Test more core functionality.

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,14 @@ int main (int argc, char ** argv) {
     ref(argc);
     ref(argv);
 
-	printf(
-		"System page size is " PF_SIZE_T " bytes\n",
-		CSaruCore::GetSystemPageSize()
-	);
+	std::size_t tempSt = CSaruCore::GetSystemPageSize();
+	std::printf("System page size is {" PF_SIZE_T "} bytes.\n", tempSt);
+
+	CSaruCore::SecureZero(&tempSt, sizeof(tempSt));
+	std::printf("Set to zero, that value is {" PF_SIZE_T "}.\n", tempSt);
+
+	std::printf("Beep!\n");
+	CSaruCore::Beep();
 
     return 0;
 
